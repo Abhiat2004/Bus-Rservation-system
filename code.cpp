7ba60a33f0d133b2b8bc4c7b2b312a577bd4b0dc
Cheedella S V Abhinava Sai <98445996+Abhiat2004@users.noreply.github.com>
2023-12-08 18:34:00 +0530

    Modification

--- code.cpp
+++ code.cpp
@@ -1,267 +1,326 @@
 #include <iostream>
-#include<stdlib.h>
-#include<dos.h>
-#include<conio.h>
+#include <vector>
+#include <string>
 using namespace std;
-int i=0,b=0;
-class Database
-{
-    public:
-    string name,age,email,mobileno,username,pass,cpass;
-    string bname,bnum,bs,be,bt;
-    int bamount,seats;
-    string pname[10],page[10],pmob[10],pgender[10];
-    int tseats,ntickets=0,nbus;
 
+class Passenger {
+public:
+    string name, age, gender, mobileNumber;
 };
 
-class Account:public Database
-{
-    public:
-    void createAcc()
-    {
-        cout<<"Enter your Name : ";
-        cin>>name;
-        cout<<"Enter your Age  : ";
-        cin>>age;
-        cout<<"Enter your E-mail : ";
-        cin>>email;
-        cout<<"Enter your Mobile number : ";
-        cin>>mobileno;
-        cout<<"Create your Username : ";
-        cin>>username;
-        loop:
-        cout<<"Create your Password : ";
-        cin>>pass;
-        cout<<"Confirm your Password : ";
-        cin>>cpass;
-        if(pass==cpass)
-            cout<<"...Account Created...";
-        else
-            goto loop;
-            i++;
+class Bus {
+public:
+    string name, number, startingPoint, endingPoint, timing;
+    int seatsAvailable;
+    double seatCost;
+
+    void displayDetails() const {
+        cout << "Bus Name: " << name << endl;
+        cout << "Bus Number: " << number << endl;
+        cout << "Starting Point: " << startingPoint << endl;
+        cout << "Ending Point: " << endingPoint << endl;
+        cout << "Bus Timing at Starting Position: " << timing << endl;
+        cout << "Number of seats Available in Bus: " << seatsAvailable << endl;
+        cout << "Cost of each Seat: " << seatCost << endl << endl;
     }
-}A[100];
+};
 
-class Install:public Database
-{
-    public:
-    void bdis()
-    {
-                cout<<"Bus Name : "<<bname<<endl;
-                cout<<"Bus Number : "<<bnum<<endl;
-                cout<<"Bus Starting Point : "<<bs<<endl;
-                cout<<"Bus Ending Point : "<<be<<endl;
-                cout<<"Bus Timing at Starting Position : "<<bt<<endl;
-                cout<<"Number of seats Available in Bus : "<<seats<<endl;
-                cout<<"Cost of each Seat : "<<bamount<<endl<<endl<<endl;
 
-    }
+class Account {
+public:
+    string name, age, email, mobileNumber, username, password, confirmPassword;
+};
 
-    void install()
-    {
-        cout<<"Bus Name : ";
-        cin>>bname;
-        cout<<"Bus Number : ";
-        cin>>bnum;
-        cout<<"Bus Starting Point : ";
-        cin>>bs;
-        cout<<"Bus Ending Point : ";
-        cin>>be;
-        cout<<"Bus Timing at Starting Position : ";
-        cin>>bt;
-        cout<<"Total number of seats in Bus : ";
-        cin>>seats;
-        cout<<"Cost of each Seat : ";
-        cin>>bamount;
-        b++;
-    }
+class Booking {
+public:
+    int busIndex;
+    vector<Passenger> passengers;
 
-}I[100];
+    void bookTicket(const Bus& bus) {
+        busIndex = -1;
+        passengers.clear();
 
-class Booking: public Database
-{
-    public:
-        int k;
-    void booking()
-    {
-        for(int c=1,k=0;k<b;k++,c++)
-                        {
-                         cout<<c<<endl;
-                         I[k].bdis();
-                         cout<<endl<<endl;
-                        }
-        cout<<"Select a Bus : ";
-        cin>>nbus;
-        cout<<"Enter number of Passengers : ";
-        cin>>tseats;
-        for(k=0;k<tseats;k++)
-        {
-            cout<<"Enter Name of Passenger "<<k+1<<" : ";
-            cin>>pname[k];
-            cout<<"Enter Age of  "<<pname[k]<<" : ";
-            cin>>page[k];
-            cout<<"Enter Gender of  "<<pname[k]<<" : ";
-            cin>>pgender[k];
-            cout<<"Enter Mobile number of  "<<pname[k]<<" : ";
-            cin>>pmob[k];
-            cout<<endl<<endl;
-        }
-        I[nbus-1].seats=I[nbus-1].seats-tseats;
-        cout<<".....Ticket is booked....."<<endl;
-    }
-}B[100];
+        cout << "Select a Bus: ";
+        cin >> busIndex;
+        cout << "Enter number of Passengers: ";
+        int totalSeats;
+        cin >> totalSeats;
 
-class Ticket:public Database
-{
-    public:
-    void showticket(int m,int s)
-    {
-        int n;
-        cout<<"Bus Name : "<<I[m].bname<<endl;
-        cout<<"Bus Number : "<<I[m].bnum<<endl;
-        cout<<"Bus Starting Point : "<<I[m].bs<<endl;
-        cout<<"Bus Ending Point : "<<I[m].be<<endl;
-        cout<<"Bus Timing at Starting Position : "<<I[m].bt<<endl;
-        cout<<"No of seats booked : "<<B[m].tseats<<endl;
-        cout<<"Total Amount : "<<I[m].bamount*B[m].tseats<<endl<<endl;
-        cout<<"Details of Passengers :"<<endl;
-        for(n=0;n<B[m].k;n++)
-        {
-            cout<<"Name of the Passenger "<<n+1<<" : "<<B[m].pname[n]<<endl;
-            cout<<"Age of "<<B[m].pname[n]<<" : "<<B[m].age[n]<<endl;
-            cout<<"Gender of "<<B[m].pname[n]<<" : "<<B[m].pgender[n]<<endl;
-            cout<<"Mobile number of "<<B[m].pname[n]<<" : "<<B[m].pmob[n]<<endl<<endl;
+        for (int i = 0; i < totalSeats; ++i) {
+            Passenger passenger;
+            cout << "Enter Name of Passenger " << i + 1 << ": ";
+            cin >> passenger.name;
+            cout << "Enter Age of " << passenger.name << ": ";
+            cin >> passenger.age;
+            cout << "Enter Gender of " << passenger.name << ": ";
+            cin >> passenger.gender;
+            cout << "Enter Mobile number of " << passenger.name << ": ";
+            cin >> passenger.mobileNumber;
+            cout << endl;
+            passengers.push_back(passenger);
         }
     }
-}T;
+};
 
-int main()
-{
-    int ch,k;
-    string aname,apass;
-    mloop:
-    cout<<"Welcome to SRM BUS Portal"<<endl;
-    cout<<"1. Admin Portal"<<endl<<"2. User Portal"<<endl<<"3. Exit"<<endl<<"Enter Your Choice : ";
-    cin>>ch;
-    system("cls");
-    if(ch==1)
-    {
-        loop:
-        cout<<"Enter Admin Username : ";
-        cin>>aname;
-        if(aname=="srmadmin")
-        {
-            loop1:
-            cout<<"Enter Admin Password : ";
-            cin>>apass;
-            if(apass=="Admin22")
-            {
-                cout<<"Welcome to Admin Portal"<<endl;
-                aloop:
-                cout<<"1. Display "<<endl<<"2. Install"<<endl<<"3. Logout"<<endl<<"Enter your choice : ";
-                cin>>ch;
-                system("cls");
-                if(ch==1)
-                    {
-                        if(b==0){
-                            cout<<"No Busses are Installed!"<<endl;delay(1000);}
-                        else{
-                        for(int j=0;j<b;j++)
-                            I[j].bdis();}
-                        goto aloop;
-                    }
-                if(ch==2)
-                {
-                    I[b].install();
-                    goto aloop;
-                }
-                if(ch==3)
-                    goto mloop;
-            }
-                else{
-                    cout<<"Invalid Password"<<endl;goto loop1;}
+class Ticket {
+public:
+    void showTicket(const Bus& bus, const Booking& booking) {
+        cout << "Bus Name: " << bus.name << endl;
+        cout << "Bus Number: " << bus.number << endl;
+        cout << "Bus Starting Point: " << bus.startingPoint << endl;
+        cout << "Bus Ending Point: " << bus.endingPoint << endl;
+        cout << "Bus Timing at Starting Position: " << bus.timing << endl;
+        cout << "Number of seats booked: " << booking.passengers.size() << endl;
+        cout << "Total Amount: " << bus.seatCost * booking.passengers.size() << endl << endl;
 
+        cout << "Details of Passengers:" << endl;
+        for (int i = 0; i < booking.passengers.size(); ++i) {
+            const Passenger& passenger = booking.passengers[i];
+            cout << "Name of the Passenger " << i + 1 << ": " << passenger.name << endl;
+            cout << "Age of " << passenger.name << ": " << passenger.age << endl;
+            cout << "Gender of " << passenger.name << ": " << passenger.gender << endl;
+            cout << "Mobile number of " << passenger.name << ": " << passenger.mobileNumber << endl << endl;
         }
-        else{
-            cout<<"Invalid Username"<<endl;
-            goto loop;}
-
     }
-    if(ch==2)
-    {
-        cout<<"1. Login"<<endl<<"2. Sign Up"<<endl<<"Enter your Choice : ";
-        cin>>ch;
-        if(ch==1)
-        {
-            lloop:
-            cout<<"Enter Username : ";
-            cin>>aname;
-            for(int j=0;j<i;j++)
-            {
-                if(A[j].username==aname)
-                {
-                     ploop:
-                cout<<"Enter Password : ";
-                cin>>apass;
-                if(apass==A[j].pass){
-                    cout<<endl<<"Welcome "<<A[j].name<<endl;
-                    tloop:
-                    cout<<"1. Book Tickets"<<endl<<"2. Show my Ticket"<<endl<<"3. Display Buses"<<endl<<"4. Logout"<<endl<<"Enter your Choice : ";
-                    cin>>ch;
-                    if(ch==1)
-                    {
-                        if(B[j].ntickets==0){
-                        B[j].booking();B[j].ntickets++;}
-                        else
-                            cout<<"...Max Limit is Exceeded..";
-                        goto tloop;
-                    }
-                    if(ch==2)
-                    {
-                        if(B[j].ntickets==0){cout<<"No Tickets are Booked"<<endl;goto tloop;}
-                        else{
-                            T.showticket(j,B[j].nbus);
-                            goto tloop;}
-                    }
-                    if(ch==3)
-                    {
-                        for(k=0;k<b;k++)
-                        {
-                         I[k].bdis();
-                         cout<<endl<<endl;
-                        }
-                        goto tloop;
-                    }
-                    if(ch==4)
-                    {
-                        goto mloop;
-                    }
-                    }
-                    else
-                    {
-                        cout<<"Invalid Password"<<endl;
-                        goto ploop;
-                    }
-                    k=1;
+};
+
+class BusPortal {
+private:
+    vector<Account> accounts;
+    vector<Bus> buses;
+    vector<Booking> bookings;
+    Ticket ticket;
+
+public:
+    void run() {
+        int choice;
+        do {
+            cout << "Welcome to SRM BUS Portal" << endl;
+            cout << "1. Admin Portal" << endl << "2. User Portal" << endl << "3. Exit" << endl;
+            cout << "Enter Your Choice: ";
+            cin >> choice;
+            system("clear");
+
+            switch (choice) {
+                case 1:
+                    adminPortal();
                     break;
-                }
-                }
-                if(k!=1){
-                    cout<<"NO Account found!";
-                    goto lloop;}
-
+                case 2:
+                    userPortal();
+                    break;
+                case 3:
+                    cout << "Exiting the program" << endl;
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
             }
+        } while (choice != 3);
+    }
 
+private:
+    void adminPortal() {
+        string adminName, adminPassword;
 
-        if(ch==2)
-        {
-            A[i].createAcc();
-            cout<<endl;
-            goto mloop;
+        cout << "Enter Admin Username: ";
+        cin >> adminName;
+
+        if (adminName == "srmadmin") {
+            cout << "Enter Admin Password: ";
+            cin >> adminPassword;
+
+            if (adminPassword == "Admin22") {
+                adminMenu();
+            } else {
+                cout << "Invalid Password" << endl;
+            }
+        } else {
+            cout << "Invalid Username" << endl;
+        }
+    }
+
+    void adminMenu() {
+        int choice;
+        do {
+            cout << "Welcome to Admin Portal" << endl;
+            cout << "1. Display Buses" << endl << "2. Install Bus" << endl << "3. Logout" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice;
+            system("clear");
+
+            switch (choice) {
+                case 1:
+                    displayBuses();
+                    break;
+                case 2:
+                    installBus();
+                    break;
+                case 3:
+                    cout << "Logging out from Admin Portal" << endl;
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+            }
+        } while (choice != 3);
+    }
+
+    void displayBuses() {
+        if (buses.empty()) {
+            cout << "No Buses are Installed!" << endl;
+        } else {
+            for (const auto& bus : buses) {
+                bus.displayDetails();
+            }
+        }
+    }
+
+    void installBus() {
+        Bus bus;
+        cout << "Bus Name: ";
+        cin >> bus.name;
+        cout << "Bus Number: ";
+        cin >> bus.number;
+        cout << "Bus Starting Point: ";
+        cin >> bus.startingPoint;
+        cout << "Bus Ending Point: ";
+        cin >> bus.endingPoint;
+        cout << "Bus Timing at Starting Position: ";
+        cin >> bus.timing;
+        cout << "Total number of seats in Bus: ";
+        cin >> bus.seatsAvailable;
+        cout << "Cost of each Seat: ";
+        cin >> bus.seatCost;
+
+        buses.push_back(bus);
+    }
+
+    void userPortal() {
+        int choice;
+        do {
+            cout << "1. Login" << endl << "2. Sign Up" << endl << "3. Exit" << endl;
+            cout << "Enter your Choice: ";
+            cin >> choice;
+            system("clear");
+
+            switch (choice) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    signUp();
+                    break;
+                case 3:
+                    cout << "Exiting User Portal" << endl;
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+            }
+        } while (choice != 3);
+    }
+
+    void login() {
+        string username, password;
+
+        cout << "Enter Username: ";
+        cin >> username;
+
+        for (const auto& account : accounts) {
+            if (account.username == username) {
+                cout << "Enter Password: ";
+                cin >> password;
+
+                if (account.password == password) {
+                    userMenu(account);
+                    return;
+                } else {
+                    cout << "Invalid Password" << endl;
+                    return;
+                }
+            }
         }
 
+        cout << "No Account found!" << endl;
     }
-    if(ch==3)
+
+    void userMenu(const Account& account) {
+        int choice;
+        Booking booking;
+
+        do {
+            cout << "Welcome " << account.name << endl;
+            cout << "1. Book Tickets" << endl << "2. Show my Ticket" << endl << "3. Display Buses" << endl << "4. Logout"
+                 << endl;
+            cout << "Enter your Choice: ";
+            cin >> choice;
+            system("clear");
+
+            switch (choice) {
+                case 1:
+                    if (booking.passengers.empty()) {
+                        booking.bookTicket(buses[booking.busIndex]);
+                        bookings.push_back(booking);
+                    } else {
+                        cout << "Max Limit is Exceeded" << endl;
+                    }
+                    break;
+                case 2:
+                    if (!booking.passengers.empty()) {
+                        ticket.showTicket(buses[booking.busIndex], booking);
+                    } else {
+                        cout << "No Tickets are Booked" << endl;
+                    }
+                    break;
+                case 3:
+                    displayBuses();
+                    break;
+                case 4:
+                    cout << "Logging out" << endl;
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+            }
+        } while (choice != 4);
+    }
+
+    void signUp() {
+        Account account;
+        cout << "Enter your Name: ";
+        cin >> account.name;
+        cout << "Enter your Age: ";
+        cin >> account.age;
+        cout << "Enter your E-mail: ";
+        cin >> account.email;
+        cout << "Enter your Mobile number: ";
+        cin >> account.mobileNumber;
+        cout << "Create your Username: ";
+        cin >> account.username;
+
+        string password, confirmPassword;
+        do {
+            cout << "Create your Password: ";
+            cin >> password;
+            cout << "Confirm your Password: ";
+            cin >> confirmPassword;
+
+            if (password != confirmPassword) {
+                cout << "Passwords do not match. Please try again." << endl;
+            }
+
+        } while (password != confirmPassword);
+
+        account.password = password;
+        accounts.push_back(account);
+
+        cout << "Account Created" << endl;
+    }
+};
+
+int main() {
+    BusPortal busPortal;
+    busPortal.run();
+
     return 0;
 }
